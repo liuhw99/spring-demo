603e85a3d7ed0e46af4d6dc20db1526b7de08895
huawei liu <huawei.liu@pointclickcare.com>
2019-08-11 20:51:17 -0400

    Result Type

---
+++ src/main/java/com/battcn/demo/controller/ExceptionController.java
@@ -0,0 +1,31 @@
+package com.battcn.demo.controller;
+
+import com.battcn.demo.exception.Result;
+import com.battcn.demo.exception.ResultTypeEnum;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @auther: chenmingyu
+ * @date: 2018/12/6 14:20
+ * @description:
+ */
+@RestController
+public class ExceptionController {
+
+    /**
+     * 成功返回值
+     */
+    @RequestMapping("/querySuccess")
+    public Result<String> querySuccess(){
+        return Result.success("我是数据");
+    }
+
+    /**
+     * 错误返回值
+     */
+    @RequestMapping("/queryError")
+    public Result<String> queryError(){
+        return Result.error(ResultTypeEnum.SERVICE_ERROR);
+    }
+}
